d7d94f283a62df18a691a2de40fbc0fc18a9eca2
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-26 13:09:01 +0430

    ✨ feat:Added products styled and component.

--- src/components/Products/Product.styled.js
+++ src/components/Products/Product.styled.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
-const StyledProduct = styled.div``;
+const StyledProduct = styled.div`
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  margin-bottom: 40px;
+`;
 
 export default StyledProduct;
--- src/components/Products/products.js
+++ src/components/Products/products.js
@@ -2,39 +2,66 @@ import React, { useEffect, useState } from "react";
 import { FaHeart, FaStore } from "react-icons/fa";
 import Flex from "../Flex.styled";
 import StyledProduct from "./Product.styled";
+import StyledImage from "../Image.styled";
 const Products = function () {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchProducts = async function () {
       const response = await fetch("https://dummyjson.com/products");
       const data = await response.json();
-      setProducts(data);
+      if (data) {
+        setLoading(false);
+        setProducts(data.products);
+      }
     };
 
     fetchProducts();
   }, []);
 
-  return (
-    <StyledProduct>
-      <img src={products.thumbnail} alt="" />
-      <p>{products.brand}</p>
-      <h3>{products.title}</h3>
-      <p>{products.description}</p>
-      <hr />
+  const htmlData = function (items, col) {
+    let htmlElements = (
       <Flex>
-        <div>
-          <span style={{ textDecoration: "line-through" }}>
-            ${products.price}
-          </span>
-          <span>${products.discountPercentage}</span>
-        </div>
-        <div>
-          <FaHeart />
-          <FaStore />
-        </div>
+        {items.map((item) => (
+          <StyledProduct key={item.id}>
+            <StyledImage src={item.thumbnail} alt="" />
+            <p>{item.brand}</p>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <hr />
+            <Flex>
+              <div>
+                <span style={{ textDecoration: "line-through" }}>
+                  {item.price}
+                </span>
+                <span>{item.discountPercentage}</span>
+              </div>
+              <div>
+                <FaHeart />
+                <FaStore />
+              </div>
+            </Flex>
+          </StyledProduct>
+        ))}
       </Flex>
-    </StyledProduct>
-  );
+    );
+
+    return htmlElements;
+  };
+
+  const renderFarm = function () {
+    const rows = [...Array(Math.ceil(products.length / 7))];
+    const productRows = rows.map((item, index) =>
+      products.slice(index * 4, index * 4 + 4)
+    );
+    const data = productRows.map((row, index) => {
+      return htmlData(row);
+    });
+    console.log(data);
+    return data;
+  };
+
+  return <>{loading ? "Data is Loading...." : renderFarm()}</>;
 };
 
 export default Products;
