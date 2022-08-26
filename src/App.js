ca4e9d6b0fe37dab9ce6c82b47abe159978b484e
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-26 17:18:29 +0430

    ♻️ refactor:Added nested route to products route.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Admin from "./components/Admin/admin";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/GlobalStyles";
+import Header from "./components/Header/Header";
 import Container from "./components/Container.styled";
-import GlobalStyles from "./components/globalStyles";
 import Products from "./components/Products/products";
+import ProductsIndex from "./components/Products/ProductsIndex";
+import Admin from "./components/Admin/admin";
 
 const App = () => {
+  const theme = {
+    colors: {
+      body: "#fff",
+      header: "#524d4d",
+    },
+    flex: {
+      spaceBetween: "space-between",
+      center: "center",
+    },
+  };
   return (
     <>
-      <Router>
-        <Routes>
-          <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Router>
+          <Header />
           <Container>
-            <Route path="/" element={<Products />} />
-            <Route path="/admin" element={<Admin />} />
+            <Routes>
+              <Route path="/" element={<Products />}>
+                <Route path="/" element={<ProductsIndex />} />
+              </Route>
+              <Route path="/admin" element={<Admin />} />
+            </Routes>
           </Container>
-        </Routes>
-      </Router>
+        </Router>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/components/Products/ProductsIndex.js
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import { FaHeart, FaStore } from "react-icons/fa";
+import Flex from "../Flex.styled";
+import StyledProduct from "./Product.styled";
+import StyledImage from "../Image.styled";
+const ProductsIndex = function () {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchProducts = async function () {
+      const response = await fetch("https://dummyjson.com/products");
+      const data = await response.json();
+      if (data) {
+        setLoading(false);
+        setProducts(data.products);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const htmlData = function (items, col) {
+    let htmlElements = (
+      <Flex>
+        {items.map((item) => (
+          <StyledProduct key={item.id}>
+            <StyledImage src={item.thumbnail} alt="" />
+            <p>{item.brand}</p>
+            <h3>{item.title}</h3>
+            <p>{item.description}</p>
+            <hr />
+            <Flex>
+              <div>
+                <span style={{ textDecoration: "line-through" }}>
+                  {item.price}
+                </span>
+                <span>{item.discountPercentage}</span>
+              </div>
+              <div>
+                <FaHeart />
+                <FaStore />
+              </div>
+            </Flex>
+          </StyledProduct>
+        ))}
+      </Flex>
+    );
+
+    return htmlElements;
+  };
+
+  const renderFarm = function () {
+    const rows = [...Array(Math.ceil(products.length / 7))];
+    const productRows = rows.map((item, index) =>
+      products.slice(index * 4, index * 4 + 4)
+    );
+    const data = productRows.map((row, index) => {
+      return htmlData(row);
+    });
+    console.log(data);
+    return data;
+  };
+
+  return <>{loading ? "Data is Loading...." : renderFarm()}</>;
+};
+
+export default ProductsIndex;
