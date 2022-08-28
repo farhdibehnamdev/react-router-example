b3489c53538ad8030b5fe26118e645d6bfd8f500
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-28 15:36:19 +0430

    ✨ feat:Added back button for each single product.

---
+++ src/components/Button/Button.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  border: none;
+  border-radius: 5px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.25);
+  background-color: aliceblue;
+  padding: 16px 55px 16px 55px;
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 16px;
+`;
+
+export default StyledButton;
--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -3,51 +3,59 @@ import StyledProduct from "./Product.styled";
 import { FaHeart, FaStore } from "react-icons/fa";
 import Flex from "../Flex.styled";
 import StyledImage from "../Image.styled";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import StyledButton from "../Button/Button.styled";
 
 const Product = function () {
   const [product, setProduct] = useState({});
   const { id } = useParams();
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async function () {
       try {
         const data = await fetch(`https://dummyjson.com/products/${id}`);
-        const response = await data.json();
-        if (response) {
-          console.log(response);
-          setProduct(response);
+        if (data.ok) {
+          const response = await data.json();
+          if (response) {
+            setProduct(response);
+          }
         } else {
-          throw new Error("fetching data went wrong...");
+          throw new Error("something wrong....");
         }
       } catch (err) {
-        console.log(err);
+        navigate("/", { replace: true, state: { id } });
       }
     };
     fetchData();
   }, [id]);
 
   return (
-    <Flex statusPosition="center">
-      <StyledProduct key={product.id}>
-        <StyledImage src={product.thumbnail} alt="" />
-        <p>{product.brand}</p>
-        <h3>{product.title}</h3>
-        <p>{product.description}</p>
-        <hr />
-        <Flex>
-          <div>
-            <span style={{ textDecoration: "line-through" }}>
-              {product.price}
-            </span>
-            <span>{product.discountPercentage}</span>
-          </div>
-          <div>
-            <FaHeart />
-            <FaStore />
-          </div>
-        </Flex>
-      </StyledProduct>
-    </Flex>
+    <>
+      <StyledButton type="button" onClick={() => navigate(-1)}>
+        Back
+      </StyledButton>
+      <Flex statusPosition="center">
+        <StyledProduct key={product.id}>
+          <StyledImage src={product.thumbnail} alt="" />
+          <p>{product.brand}</p>
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <hr />
+          <Flex>
+            <div>
+              <span style={{ textDecoration: "line-through" }}>
+                {product.price}
+              </span>
+              <span>{product.discountPercentage}</span>
+            </div>
+            <div>
+              <FaHeart />
+              <FaStore />
+            </div>
+          </Flex>
+        </StyledProduct>
+      </Flex>
+    </>
   );
 };
 
