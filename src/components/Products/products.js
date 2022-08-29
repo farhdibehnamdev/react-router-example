45142d5ff93471c28a1d0eb62bb5d74460ad440a
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-29 12:20:57 +0430

    ♻️ refactor:Instead of using outle i use route.

--- src/components/Products/products.js
+++ src/components/Products/products.js
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Product from "../Products/Product";
+import ProductsIndex from "../Products/ProductsIndex";
 const Products = function () {
-  return <Outlet />;
+  return (
+    <Routes>
+      <Route path="/" element={<ProductsIndex />} />
+      <Route path="product/:id" element={<Product />} />
+    </Routes>
+  );
 };
 
 export default Products;
