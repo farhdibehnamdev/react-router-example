c64c34c1d5fb3042a382bd1f5fe460446bbbf0d1
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-30 10:36:11 +0430

    💄 Added TextContainer styles.

--- src/components/Products/ProductCard.js
+++ src/components/Products/ProductCard.js
@@ -2,6 +2,7 @@ import { FaHeart, FaStore } from "react-icons/fa";
 import Flex from "../Flex.styled";
 import StyledProduct from "./Product.styled";
 import StyledImage from "../Image.styled";
+import TextContainer from "../TextContainer.styled";
 import { Link } from "react-router-dom";
 
 const ProductCard = function ({ products }) {
@@ -10,23 +11,29 @@ const ProductCard = function ({ products }) {
       {products.map((item) => (
         <Link key={item.id} to={`product/${item.id}`}>
           <StyledProduct key={item.id}>
-            <StyledImage src={item.thumbnail} alt="" />
-            <p>{item.brand}</p>
-            <h3>{item.title}</h3>
-            <p>{item.description}</p>
-            <hr />
-            <Flex>
-              <div>
-                <span style={{ textDecoration: "line-through" }}>
-                  {item.price}
-                </span>
-                <span>{item.discountPercentage}</span>
-              </div>
-              <div>
-                <FaHeart />
-                <FaStore />
-              </div>
-            </Flex>
+            <StyledImage src={`./assets/images/${item.id}.jpg`} alt="" />
+            <TextContainer>
+              <p>{item.brand}</p>
+              <h3>{item.title}</h3>
+              <p>
+                {item.description.length > 30
+                  ? `${item.description.slice(0, 30)}...`
+                  : item.description}
+              </p>
+              <hr />
+              <Flex>
+                <div>
+                  <span style={{ textDecoration: "line-through" }}>
+                    {item.price}
+                  </span>
+                  <span>{item.discountPercentage}</span>
+                </div>
+                <div>
+                  <FaHeart />
+                  <FaStore />
+                </div>
+              </Flex>
+            </TextContainer>
           </StyledProduct>
         </Link>
       ))}
