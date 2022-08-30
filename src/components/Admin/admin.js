b27d9a61d535fccd0547e392bb48dd3eac1ea8e8
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-08-30 10:34:55 +0430

    ✨feat: Added new and edit product in admin page.

--- src/components/Admin/admin.js
+++ src/components/Admin/admin.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import ProductsIndex from "../Products/ProductsIndex";
+import ProductEdit from "../Products/ProductEdit";
 const Admin = function () {
-  return <div>this is admin</div>;
+  return (
+    <>
+      <h1>Admin</h1>
+      <Routes>
+        <Route path="/" element={<ProductsIndex />} />
+        <Route path="/new" element={<ProductEdit />} />
+      </Routes>
+    </>
+  );
 };
 
 export default Admin;
---
+++ src/components/Products/ProductEdit.js
@@ -0,0 +1,5 @@
+const ProductEdit = function () {
+  return <div>I am a product</div>;
+};
+
+export default ProductEdit;
