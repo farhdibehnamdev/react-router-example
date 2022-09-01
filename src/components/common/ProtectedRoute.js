e1df193df5942ecbc3ca9a564e3b763826fdfacf
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-09-01 17:32:53 +0430

    ♻️ refactor:Propped down children instead of using Routes.

--- src/components/common/ProtectedRoute.js
+++ src/components/common/ProtectedRoute.js
@@ -1,18 +1,6 @@
-import { Routes, Route, Navigate, Outlet } from "react-router-dom";
-import Admin from "../Admin/admin";
-
-const ProtectedRoute = function ({ redirectTo }) {
-  console.log(redirectTo);
+import { Navigate } from "react-router-dom";
+const ProtectedRoute = function ({ redirectTo, children }) {
   let authenticated = { auth: true };
-  // return authenticated.auth ? <Outlet /> : <Navigate to={redirectTo} />;
-
-  if (!authenticated.auth) return <Navigate to={redirectTo} />;
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Admin />} />
-      </Routes>
-    </>
-  );
+  return authenticated.auth ? children : <Navigate to={redirectTo} />;
 };
 export default ProtectedRoute;
