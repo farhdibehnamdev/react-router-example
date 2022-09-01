41d1038ca714219b83001cc57041163a5e66b88c
farhdibehnamdev <97886945+farhdibehnamdev@users.noreply.github.com>
2022-09-01 17:40:14 +0430

    ✨ feat:Added Edit and create product feature.

--- src/components/Admin/admin.js
+++ src/components/Admin/admin.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import ProductsIndex from "../Products/ProductsIndex";
 import ProductEdit from "../Products/ProductEdit";
+import Flex from "../Flex.styled";
+import StyledLink from "../Link.styled";
 const Admin = function () {
+  const location = useLocation();
   return (
     <>
-      <h1>Admin</h1>
+      <Flex>
+        <h1>Admin</h1>
+        <StyledLink to="new">New</StyledLink>
+      </Flex>
+      <br />
       <Routes>
         <Route path="/" element={<ProductsIndex />} />
         <Route path="/new" element={<ProductEdit />} />
+        <Route path="/:id" element={<ProductEdit />} />
       </Routes>
     </>
   );
--- src/components/Products/ProductEdit.js
+++ src/components/Products/ProductEdit.js
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {
+  addProduct,
+  getProduct,
+  updateProduct,
+} from "../../services/ProductServices";
+import Form from "../Form/Form";
+
 const ProductEdit = function () {
-  return <div>I am a product</div>;
+  const [form, setForm] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setForm({
+      title: "",
+      price: 0,
+      description: "",
+    });
+
+    const fetchData = async function () {
+      try {
+        const product = await getProduct(id);
+        setForm(product);
+      } catch (error) {
+        console.warn(error);
+        navigate(`/admin`, { replace: true });
+      }
+    };
+    fetchData();
+  }, []);
+
+  const updateField = function ({ name, value }) {
+    setForm({ ...form, [name]: value });
+  };
+
+  const addProductHandler = async function () {
+    const response = await addProduct(form);
+    const result = await response.json();
+    if (result) {
+      navigate(`/admin/${result.id}`);
+    }
+  };
+
+  const updateProductHandler = async function () {
+    try {
+      console.log("updateProductHandler :::", form);
+      await updateProduct(form);
+      alert(`Updated ${form.title}`);
+      navigate(`/admin/${id}`);
+    } catch (err) {
+      console.warn(err);
+    }
+  };
+
+  return (
+    <Form
+      addProduct={() => addProductHandler()}
+      updateProduct={() => updateProductHandler()}
+      form={form}
+      updateField={updateField}
+    />
+  );
 };
 
 export default ProductEdit;
